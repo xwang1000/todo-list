1966049ef3bfce313541aad2b7607c8c5de4abad
xwang3000 <xwang3000@gmail.com>
2018-08-14 15:06:29 -0700

    Remove default component, replace it with TodoList.

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,15 +1,15 @@
 <template>
-  <div id="app">
-    <router-view/>
+  <div>
+    Hello todo list
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App'
+  
 }
 </script>
 
 <style>
-
+  
 </style>
