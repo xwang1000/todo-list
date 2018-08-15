223cfc64ac1b9b8835810538b2947ab06e6c228c
xwang3000 <xwang3000@gmail.com>
2018-08-14 17:19:15 -0700

    Create a component for individual todo item.

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <li class="todo-item">
+    {{todoItem.description}}
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    todoItem: Object
+  }
+}
+</script>
+
+<style>
+
+</style>
